0b7f6d41cfc1afdad47130040aa85081b515b8fc
drainuzzo <drainuzzo@users.noreply.github.com>
2022-03-15 10:38:23 +0100

    first commit
    go

---
+++ fastPortChecker.ps1
@@ -0,0 +1,15 @@
+function testport ($hostname=$args[0],[int[]]$Port=$args[1],$timeout=200) {
+
+    ForEach ($P  in $Port)  {
+      $requestCallback = $state = $null
+      $client = New-Object System.Net.Sockets.TcpClient
+      $beginConnect = $client.BeginConnect($hostname,$P,$requestCallback,$state)
+      Start-Sleep -milli $timeOut
+      if ($client.Connected) { $open = $true } else { $open = $false }
+      $client.Close()
+      [pscustomobject]@{hostname=$hostname;port=$P;open=$open}
+    }
+}
+
+
+testport testport $args[0] $args[1]
